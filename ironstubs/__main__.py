c1d8dc366c3c29beb3b082f8326cebb5f0910630
Leron Gray <daddycocoaman@gmail.com>
2021-10-14 07:17:12 -0700

    add all option for provided path

--- ironstubs/__main__.py
+++ ironstubs/__main__.py
@@ -21,11 +21,14 @@ Note:
 MIT LICENSE
 https://github.com/gtalarico/ironpython-stubs
 Gui Talarico
+
+Further modified by daddycocoaman, just a little.
 """
 
 import sys
 import os
 import json
+import glob
 
 from utils.docopt import docopt
 from utils.logger import logger
@@ -33,7 +36,7 @@ from utils.helper import Timer
 from default_settings import PATHS, BUILTINS, ASSEMBLIES
 from make_stubs import make, dump_json_log
 
-__version__ = '1.0.0'
+__version__ = "1.0.0"
 __doc__ = """
     IronPython-Stubs | {version}
 
@@ -49,39 +52,43 @@ __doc__ = """
 
     Options:
         <assembly-name>         Name of Dll Assembly to load
-        --all                   Process all Assemblies in the default_settings.py
+        --all                   Process all Assemblies
 
         --output=<dir>          Name of Output Directory [default: {out_dir}]
         --path=<dir>            Additional Directory to add to Path [default: ]
         --overwrite             Force Overwrite if stub already exists [default: False].
         --no-json               Disables Json Log
         --debug                 Enables Debug Messages
+        --quiet                 Quiet Mode
         -h, --help              Show this screen.
 
-    """.format(out_dir='stubs', version=__version__)
+    """.format(
+    out_dir="stubs", version=__version__
+)
 
 arguments = docopt(__doc__, version=__version__)
 
 # OPTIONS
-option_assembly_name = arguments['<assembly-name>']
-option_all = arguments['--all']
+option_assembly_name = arguments["<assembly-name>"]
+option_all = arguments["--all"]
 
-option_output_dir = arguments['--output']
-option_path_dir = arguments['--path']
-option_json = not arguments['--no-json']
-option_overwrite = arguments['--overwrite']
+option_output_dir = arguments["--output"]
+option_path_dir = arguments["--path"]
+option_json = not arguments["--no-json"]
+option_overwrite = arguments["--overwrite"]
+option_quiet = arguments["--quiet"]
 
-if arguments['--debug']:
+if arguments["--debug"]:
     logger.enable_debug()
 
 # PROJECT_DIR = os.getcwd()  # Must execute from project dir
 PKG_DIR = os.path.dirname(__file__)
 PROJECT_DIR = os.path.dirname(PKG_DIR)
-release_dir = os.path.join(PROJECT_DIR, 'release', option_output_dir)
+release_dir = os.path.join(PROJECT_DIR, "release", option_output_dir)
 os.chdir(PROJECT_DIR)
 
 # Add Paths
-PATHS.append(os.path.join(PROJECT_DIR, 'bin'))
+PATHS.append(os.path.join(PROJECT_DIR, "bin"))
 [sys.path.append(p) for p in PATHS]
 
 # Additional Paths from Options
@@ -91,14 +98,19 @@ if option_path_dir:
 logger.debug(sys.path)
 logger.debug(arguments)
 logger.debug(ASSEMBLIES)
-if arguments['make']:
+if arguments["make"]:
     timer = Timer()
     if not option_all:
         ASSEMBLIES = [option_assembly_name]
+    elif option_all and option_path_dir:
+        ASSEMBLIES = [
+            file[:-4] for file in os.listdir(option_path_dir) if file.endswith(".dll")
+        ]
 
     for assembly_name in ASSEMBLIES:
-        assembly_dict = make(release_dir, assembly_name,
-                             overwrite=option_overwrite, quiet=option_all)
+        assembly_dict = make(
+            release_dir, assembly_name, overwrite=option_overwrite, quiet=option_all
+        )
         if option_json:
             dump_json_log(assembly_dict)
-    logger.info('Done: {} seconds'.format(timer.stop()))
+    logger.info("Done: {} seconds".format(timer.stop()))
